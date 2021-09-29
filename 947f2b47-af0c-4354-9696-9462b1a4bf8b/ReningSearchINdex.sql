62028181eb6f6a28fdf7f3f2ac2649a108b0066e
perXautomatik <christoffer.broback@gmail.com>
2021-09-29 16:30:04 +0200

    asd

--- 947f2b47-af0c-4354-9696-9462b1a4bf8b/ReningSearchINdex.sql
+++ 947f2b47-af0c-4354-9696-9462b1a4bf8b/ReningSearchINdex.sql
@@ -1,12 +1,12 @@
 with reningarUNion      as (
-select 'M' f ,OBJECTID, Fastighet_rening, Antal_hushall_rening, reningstyp, Storlek_m2, Beslut_datum, Utford_datum, Kommentarer, Anslutna_fastigheter_1, Anslutna_fastigheter_2, Anslutna_fastigheter_3, Anslutna_fastigheter_4, Anslutna_fastigheter_5, Anslutna_fastigheter_6, Anslutna_fastigheter_7, Anslutna_fastigheter_8, Anslutna_fastigheter_9, Anslutna_fastigheter_10
-from sde_miljo_halsoskydd.gng.RENING_MELLERSTA_P union
-select 'N' f ,OBJECTID, Fastighet_rening, Antal_hushall_rening, reningstyp, Storlek_m2, Beslut_datum, Utford_datum, Kommentarer, Anslutna_fastigheter_1, Anslutna_fastigheter_2, Anslutna_fastigheter_3, Anslutna_fastigheter_4, Anslutna_fastigheter_5, Anslutna_fastigheter_6, Anslutna_fastigheter_7, Anslutna_fastigheter_8, Anslutna_fastigheter_9, Anslutna_fastigheter_10
-from sde_miljo_halsoskydd.gng.RENING_NORRA_P union
-select 'S' f ,OBJECTID, Fastighet_rening, Antal_hushall_rening, reningstyp, Storlek_m2, Beslut_datum, Utford_datum, Kommentarer, Anslutna_fastigheter_1, Anslutna_fastigheter_2, Anslutna_fastigheter_3, Anslutna_fastigheter_4, Anslutna_fastigheter_5, Anslutna_fastigheter_6, Anslutna_fastigheter_7, Anslutna_fastigheter_8, Anslutna_fastigheter_9, Anslutna_fastigheter_10
+select 'M' f ,*
+from sde_miljo_halsoskydd.gng.RENING_MELLERSTA_P union all
+select 'N' f ,*
+from sde_miljo_halsoskydd.gng.RENING_NORRA_P union all
+select 'S' f ,*
 from sde_miljo_halsoskydd.gng.RENING_SODRA_P)
 
-,fkopl as (
+,fObjAnslutFast as (
 	select f,OBJECTID, nullif(Fastighet_rening,'') fastighetsKoppling from reningarUNion
 --union 	select OBJECTID, nullif(Kommentarer,'') from reningarUNion
 union 	select f,OBJECTID, nullif(Anslutna_fastigheter_1,'') from reningarUNion
@@ -20,23 +20,23 @@ union 	select f,OBJECTID, nullif(Anslutna_fastigheter_8,'') from reningarUNion
 union 	select f,OBJECTID, nullif(Anslutna_fastigheter_9,'') from reningarUNion
 union 	select f,OBJECTID, nullif(Anslutna_fastigheter_10,'') from reningarUNion
 )
+--, mOstKopplad as (
+--select count(objectid) c, fastighetsKoppling from fkopl where fastighetsKoppling is not null group by fastighetsKoppling order by c desc
+--)
+,badFastighetsbeteckningar as (select ('-') q union select ( '.')union select ('0'))
 
-,bad as (select top 100 * from fkopl where len(fastighetsKoppling) > 1 and fastighetsKoppling is not null
-order by fastighetsKoppling)
+select * from reningarUNion inner join fObjAnslutFast on fObjAnslutFast.f=reningarUNion.f AND fObjAnslutFast.OBJECTID = reningarUNion.OBJECTID  inner join badFastighetsbeteckningar t on t.q = fObjAnslutFast.fastighetsKoppling
+
+
+--,bad as (select top 100 * from fkopl where len(fastighetsKoppling) > 1 and fastighetsKoppling is not null
+--order by fastighetsKoppling)
 
 --select * from bad
 
-select * from
-(
---    select * from sde_miljo_halsoskydd.gng.RENING_SODRA_P
---union all
---select * from sde_miljo_halsoskydd.gng.RENING_NORRA_P
---union all
-select * from sde_miljo_halsoskydd.gng.RENING_MELLERSTA_P
-) as q
 
-where objectid in (
-2802,
-3380
-)
-;
+
+--where objectid in (
+--2802,
+--3380
+--)
+--;
