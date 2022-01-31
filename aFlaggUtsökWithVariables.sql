74a6b3b2b4f36c47fbb5460e639988e287547568
perXautomatik <christoffer.broback@gmail.com>
2022-01-31 10:57:36 +0100

    now with test against flaggskikt, improved 1:1 relationship between anläggningar and fastigheter, ready to generate blåa flaggor

--- aFlaggUtsökWithVariables.sql
+++ aFlaggUtsökWithVariables.sql
@@ -1,7 +1,5 @@
-declare @rodDatum datetime = DATETIME2FROMPARTS(2006, 10, 1, 1, 1, 1, 1, 1);
-declare @socknar table (socken nvarchar(70))
-
-insert into @socknar select * from (Select N'Källunge' "socken"
+declare @rodDatum datetime = DATETIME2FROMPARTS(2006, 10, 1, 1, 1, 1, 1, 1);declare @socknar table (socken nvarchar(70))
+;insert into @socknar select * from (Select N'Källunge' "socken"
      Union
      Select 'Vallstena' as alias2
      Union
@@ -13,29 +11,27 @@ insert into @socknar select * from (Select N'Källunge' "socken"
      Union
      Select 'Stenkyrka' as alias2345
      )asdasd
-;
+;declare @fastighetsYtor table (SockenX nvarchar(70), FAStighet nvarchar(100), Shape geometry); declare @byggs table (Fastighetsbeteckning nvarchar(100), Byggnadstyp nvarchar(100), bygTot int,flagga geometry); declare @slamz table (statusx nvarchar(30), FAStighet nvarchar(100), Diarienummer nvarchar(100), Fastighet_tillstand nvarchar(150), Beslut_datum datetime, utförddatum datetime, Anteckning nvarchar(254), AnlaggningsPunkt geometry); declare @egetomh table (fastighet nvarchar(100), egetOmhändertangandeInfo nvarchar(4000)); declare @va table (fastighet nvarchar(100), typ nvarchar(273))
+;with fasWithShape as
+	(select fa.FNR,fa.BETECKNING , fa.TRAKT, yt.Shape from sde_geofir_gotland.gng.FA_FASTIGHET fa
+	inner join sde_gsd.gng.REGISTERENHET_YTA yt on fa.FNR = yt.FNR_FDS)
 
-declare @fastighetsYtor table (SockenX nvarchar(70), FAStighet nvarchar(100), Shape geometry);
-declare @byggs table (Fastighetsbeteckning nvarchar(100), Byggnadstyp nvarchar(100), bygTot int,flagga geometry);
+   ,fasInnomSocken as (SELECT socken SockenX,BETECKNING FAStighet, Shape from  fasWithShape inner join
+	 @socknar socknarOfIntresse
+	     on left(fasWithShape.TRAKT, len(socknarOfIntresse.socken)) = socknarOfIntresse.socken )
 
-declare @slamz table (statusx nvarchar(30), FAStighet nvarchar(100), Diarienummer nvarchar(100), Fastighet_tillstand nvarchar(150), Beslut_datum datetime, utförddatum datetime, Anteckning nvarchar(254), AnlaggningsPunkt geometry);
-declare @egetomh table (fastighet nvarchar(100), egetOmhändertangandeInfo nvarchar(4000));
-declare @va table (fastighet nvarchar(100), typ nvarchar(273));
-
-insert into @fastighetsYtor select * from (SELECT socken SockenX,BETECKNING FAStighet, Shape from (select fa.FNR,fa.BETECKNING , fa.TRAKT, yt.Shape from sde_geofir_gotland.gng.FA_FASTIGHET fa inner join sde_gsd.gng.REGISTERENHET_YTA yt on fa.FNR = yt.FNR_FDS) x inner join
-     @socknar socknarOfIntresse  on left(x.TRAKT, len(socknarOfIntresse.socken)) = socknarOfIntresse.socken ) qtz
-;
+insert into @fastighetsYtor select * from fasInnomSocken;
 --byggs
-with fastighetsYtor as (select * from  @fastighetsYtor)
- ,byggnad_yta as (select andamal1 Byggnadstyp, Shape from sde_gsd.gng.BYGGNAD),
-        q as (Select Byggnadstyp, fastighetsYtor.fastighet Fastighetsbeteckning, byggnad_yta.SHAPE from byggnad_yta
+;with fastighetsYtor      as (select * from  @fastighetsYtor)
+ ,   byggnad_yta         as (select andamal1 Byggnadstyp, Shape from sde_gsd.gng.BYGGNAD),
+     q                   as (Select Byggnadstyp, fastighetsYtor.fastighet Fastighetsbeteckning, byggnad_yta.SHAPE from byggnad_yta
             inner join fastighetsYtor on byggnad_yta.Shape.STWithin(fastighetsYtor.shape) = 1)
-,byggs as (        select Fastighetsbeteckning, Byggnadstyp,bygTot,shape
-	from (select *, count(shape) over (partition by Fastighetsbeteckning) bygTot, row_number() over (partition by Fastighetsbeteckning order by Byggnadstyp ) orderz from q) z     where orderz = 1 )
-insert into @byggs select * from byggs
-
-;
-with      fastighetsYtor    as (select * from  @fastighetsYtor)
+,    withRownr           as (select *, count(shape) over (partition by Fastighetsbeteckning) bygTot, row_number() over (partition by Fastighetsbeteckning order by Byggnadstyp ) orderz from q)
+   , OnlyOnePerFastighet as (        select Fastighetsbeteckning, Byggnadstyp,bygTot,shape
+	from  withRownr     where orderz = 1 )
+insert into @byggs select * from OnlyOnePerFastighet
+--select * from @byggs
+;with      fastighetsYtor    as (select * from  @fastighetsYtor)
 	, socknarOfIntresse as (select * from @socknar)
         , AnSoMedSocken     as (select left(Fastighet_tillstand, IIF(charindex(' ', Fastighet_tillstand) = 0, len(Fastighet_tillstand) + 1, charindex(' ', Fastighet_tillstand)) - 1) socken,Diarienummer,Fastighet_tillstand z,Beslut_datum,Utford_datum,Anteckning,Shape anlShape from sde_miljo_halsoskydd.gng.ENSKILT_AVLOPP_sodra_P)
         , AnNoMedSocken     as (select left(Fastighet_tillstand, IIF(charindex(' ', Fastighet_tillstand) = 0, len(Fastighet_tillstand) + 1, charindex(' ', Fastighet_tillstand)) - 1) socken,Diarienummer,Fastighet_tillstand z,Beslut_datum,Utford_datum,Anteckning,Shape anlShape from sde_miljo_halsoskydd.gng.ENSKILT_AVLOPP_Norra_P)
@@ -48,14 +44,13 @@ with      fastighetsYtor    as (select * from  @fastighetsYtor)
        			  IIF(Beslut_datum < @rodDatum, 1, 0) +	IIF(Utford_datum < @rodDatum, 1, 0) +
 			  IIF(Utford_datum is null, 1, 0)) > 0 , N'röd', 'ok') statusx,
 	  FAStighet,Diarienummer,Fastighet_tillstand,Beslut_datum,Utford_datum "utförddatum",Anteckning,anlShape AnlaggningsPunkt from SammanSlagna)
-
-
-insert into @slamz select *from slamz
-;
+	,withRownr as (select *, row_number() over (partition by fastighet order  by coalesce(utförddatum,Beslut_datum) desc) as x from slamz)
+	,OnePerFastighet as (select statusx, FAStighet, Diarienummer, Fastighet_tillstand, Beslut_datum, utförddatum, Anteckning, AnlaggningsPunkt
+			     from withRownr where x = 1)
+insert into @slamz
+select * from OnePerFastighet --this should be one per fastighet, but what about houses?
 --select * from @slamz where FAStighet = N'Hörsne nybjärs 1:20'
-;
-
-   with fastighetsYtor as (select * from  @fastighetsYtor)
+; with fastighetsYtor as (select * from  @fastighetsYtor)
       , LOKALT_SLAM_P as (select Diarienr, (case
 	   when charindex(':', Fastighet_) <> 0 then Fastighet_
 	   when charindex(':', Fastighe00) <> 0 then Fastighe00
@@ -91,17 +86,11 @@ insert into @slamz select *from slamz
     FROM egetOmhx
     WHERE (egetOmhx.FAStighet = r.FAStighet)
     FOR XML PATH(''),TYPE).value('(./text())[1]','VARCHAR(MAX)')
-  ,1,2,'') AS vaTyp
-
-			 from egetOmhx r)
-
-
-
+  ,1,2,'') AS vaTyp from egetOmhx r)
    insert into @egetomh
 select * from egetOmhy
 --where FAStighet = 'DALHEM GANDARVE 1:2'
-;
-   with fastighetsYtor as (select * from  @fastighetsYtor)
+; with fastighetsYtor as (select * from  @fastighetsYtor)
     ,planOmr as   (select shape,dp_i_omr,planprog,planansokn from sde_VA.gng.Va_planomraden_171016),
     spillAvtalGemPlanAnsok as (
 	select shape, concat(typkod,':',status,'(spill)') typ
@@ -120,14 +109,68 @@ select * from egetOmhy
     FROM vax
     WHERE (vax.FAStighet = r.FAStighet)
     FOR XML PATH(''),TYPE).value('(./text())[1]','VARCHAR(MAX)')
-  ,1,2,'') AS vaTyp
-
-			 from vax r)
-
+  ,1,2,'') AS vaTyp			 from vax r)
 insert into @va
    select * from va
    --where FAStighet = 'DALHEM GANDARVE 1:2'
+;;--  case when  statusx = 'röd' OR (statusx is null AND Byggnadstyp is not null and typ <> 'Spillvatten:Antaget(spill)' AND typ <> 'Avlopp:Antaget(spill)') then 1 else 0 end included, case when Byggnadstyp is not null then 1 else 0 end Bytyp,case when (isnull(typ,'') <> 'Spillvatten:Antaget(spill)' AND isnull(typ,'') <> 'Avlopp:Antaget(spill)') then 1 else 0 end SpillStat,
+with toTeamVatten as (
+select
+      --  case when  statusx = 'röd' OR (statusx is null AND Byggnadstyp is not null and typ <> 'Spillvatten:Antaget(spill)' AND typ <> 'Avlopp:Antaget(spill)') then 1 else 0 end included, case when Byggnadstyp is not null then 1 else 0 end Bytyp,case when (isnull(typ,'') <> 'Spillvatten:Antaget(spill)' AND isnull(typ,'') <> 'Avlopp:Antaget(spill)') then 1 else 0 end SpillStat,
+      statusx,SockenX, coalesce(fastighetsYtor.FAStighet, egetOmh.fastighet, va.fastighet, Fastighetsbeteckning,slamz.FAStighet) fastighet,
+       Diarienummer, Fastighet_tillstand, nullif(cast(Beslut_datum as date),'1988-01-01') Beslut_datum, nullif(cast(utförddatum as date),'1988-01-01') utförddatum, Anteckning
+     , egetOmh.egetOmhändertangandeInfo
+     , typ VAantek
+     , Byggnadstyp typ, Byggnadstyp , bygTot,coalesce(AnlaggningsPunkt,flagga).STPointN(1)  flagga
+from @fastighetsYtor fastighetsYtor
+full outer join @slamz slamz on slamz.FAStighet = fastighetsYtor.FAStighet
+full outer join @egetOmh egetOmh on fastighetsYtor.FAStighet = egetOmh.FAStighet
+full outer join @va va on fastighetsYtor.FAStighet = va.FAStighet
+full outer join @byggs byggs on fastighetsYtor.FAStighet = byggs.Fastighetsbeteckning
+
+--where fastighetsYtor.FAStighet like '%ekeskogs 1:6' OR fastighetsYtor.FAStighet like '%ekeskogs 1:7'
+--order by included desc, statusx, SpillStat,Bytyp
+)
+   ,correctTypOVa as (select *,case when Byggnadstyp is not null and (isnull(typ,'') <> 'Spillvatten:Antaget(spill)'
+		      AND isnull(typ,'') <> 'Avlopp:Antaget(spill)') then 'true' end harbyggtypInteIVa from toTeamVatten)
+    ,fs as (Select q.FAStighet, Status from sde_miljo_halsoskydd.gng.FLAGGSKIKTET_P byggnad_yta inner join @fastighetsYtor q on byggnad_yta.Shape.STWithin(q.shape) = 1)
+   ,FiltreraMotFlaggskiktet as(
+	select
+	   case when (statusx = 'röd' OR (statusx is null AND  harbyggtypInteIVa  = 'true' ))
+	    then 'x' end IrodUtsokning,
+	   case when (fs.fastighet not in(select fastighet from toTeamVatten where statusx = 'röd') and  harbyggtypInteIVa  = 'true' )
+		then 'x' end IgronUtsokning
+	    ,case when isnull(harbyggtypInteIVa,'nej') = 'true' then 'nej' else 'ja' end bla
+	 , statusx, SockenX, ttv.fastighet, Diarienummer, Fastighet_tillstand, Beslut_datum, utförddatum, Anteckning, egetOmhändertangandeInfo, VAantek, typ,
+	   Byggnadstyp, bygTot, flagga
+from
+     correctTypOVa ttv left outer join
+         fs on fs.FASTIGHET = ttv.fastighet
+
+where fs.Status is null and flagga is not null
+
+)
+select fastighet, concat('röd:',isnull(IrodUtsokning,'inteRöd'),',Va:',bla)
+from FiltreraMotFlaggskiktet
 ;
+
+select distinct count(*) over ( partition by IrodUtsokning,IgronUtsokning,bla) c,IrodUtsokning,IgronUtsokning,bla
+from FiltreraMotFlaggskiktet
+order by bla,IgronUtsokning,IrodUtsokning,statusx,Byggnadstyp, coalesce(statusx, Diarienummer, Fastighet_tillstand, try_cast(Beslut_datum as varchar), try_cast(utförddatum as varchar),
+    Anteckning, egetOmhändertangandeInfo, VAantek, typ, Byggnadstyp, try_cast( bygTot as varchar)
+)
+--order by statusx desc,fastighet,typ,egetOmhändertangandeInfo
+--;
+--select count(*) c, 'slam' a from @slamz union select count(*) c, 'egetomh' a  from @egetomh union select count(*) c, 'va' a  from @va union select count(*) c, 'byggs' a  from @byggs
+
+
+
+
+
+
+
+
+
 
 declare @grönaFörFlaggskikt  table (
 				 FASTIGHET                                     nvarchar(150),
@@ -160,29 +203,9 @@ declare @rödaFörFlaggskikt  table (
                                 y						bigint
 )
 
-;--  case when  statusx = 'röd' OR (statusx is null AND Byggnadstyp is not null and typ <> 'Spillvatten:Antaget(spill)' AND typ <> 'Avlopp:Antaget(spill)') then 1 else 0 end included, case when Byggnadstyp is not null then 1 else 0 end Bytyp,case when (isnull(typ,'') <> 'Spillvatten:Antaget(spill)' AND isnull(typ,'') <> 'Avlopp:Antaget(spill)') then 1 else 0 end SpillStat,
-with toTeamVatten as (
-select
-              --  case when  statusx = 'röd' OR (statusx is null AND Byggnadstyp is not null and typ <> 'Spillvatten:Antaget(spill)' AND typ <> 'Avlopp:Antaget(spill)') then 1 else 0 end included, case when Byggnadstyp is not null then 1 else 0 end Bytyp,case when (isnull(typ,'') <> 'Spillvatten:Antaget(spill)' AND isnull(typ,'') <> 'Avlopp:Antaget(spill)') then 1 else 0 end SpillStat,
-              statusx,SockenX, coalesce(fastighetsYtor.FAStighet, egetOmh.fastighet, va.fastighet, Fastighetsbeteckning,slamz.FAStighet) fastighet,
-       Diarienummer, Fastighet_tillstand, nullif(cast(Beslut_datum as date),'1988-01-01') Beslut_datum, nullif(cast(utförddatum as date),'1988-01-01') utförddatum, Anteckning
-     , egetOmh.egetOmhändertangandeInfo
-     , typ VAantek
-     , Byggnadstyp, bygTot,flagga.STPointN(1)  flagga
-from @fastighetsYtor fastighetsYtor
-full outer join @slamz slamz on slamz.FAStighet = fastighetsYtor.FAStighet
-full outer join @egetOmh egetOmh on fastighetsYtor.FAStighet = egetOmh.FAStighet
-full outer join @va va on fastighetsYtor.FAStighet = va.FAStighet
-full outer join @byggs byggs on fastighetsYtor.FAStighet = byggs.Fastighetsbeteckning
---where fastighetsYtor.FAStighet like '%ekeskogs 1:6' OR fastighetsYtor.FAStighet like '%ekeskogs 1:7'
 
-      --
-      --order by included desc, statusx, SpillStat,Bytyp
-)
---order by statusx desc,fastighet,typ,egetOmhändertangandeInfo
---;
---select count(*) c, 'slam' a from @slamz union select count(*) c, 'egetomh' a  from @egetomh union select count(*) c, 'va' a  from @va union select count(*) c, 'byggs' a  from @byggs
-
+with alias as (select * from toTeamVatten where
+    (statusx = 'röd' OR (statusx is null AND  Byggnadstyp is not null and (isnull(typ,'') <> 'Spillvatten:Antaget(spill)' AND isnull(typ,'') <> 'Avlopp:Antaget(spill)'))))
 insert into @rödaFörFlaggskikt (FASTIGHET, Fastighet_tillstand, Arendenummer, Beslut_datum, Status, Utskick_datum, Anteckning
 , Utforddatum, Slamhamtning, Antal_byggnader,x,y)
 select
@@ -196,9 +219,7 @@ select
      utförddatum
      ,left(concat( egetOmhändertangandeInfo,nullif(concat(' va: ',VAantek),' va: '), nullif(concat(' bygtyp: ' , Byggnadstyp),' bygtyp: ')),100) VAantek
      , bygTot,flagga.STX flaggax,flagga.STY flaggay
-
-from toTeamVatten
-(statusx = 'röd' OR (statusx is null AND  Byggnadstyp is not null and (isnull(typ,'') <> 'Spillvatten:Antaget(spill)' AND isnull(typ,'') <> 'Avlopp:Antaget(spill)')))
+from alias
 
 
 insert into @grönaFörFlaggskikt (FASTIGHET, Fastighet_tillstand, Arendenummer, Beslut_datum, Status, Utskick_datum, Anteckning
