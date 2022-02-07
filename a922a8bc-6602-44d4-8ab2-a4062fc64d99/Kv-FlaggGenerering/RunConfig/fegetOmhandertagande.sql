85e07fab763bafdfcda96f30e9b3f11c305777dc
perXautomatik <christoffer.broback@gmail.com>
2022-02-07 12:48:45 +0100

    Now runns, eget omhän still takes bit of time but nothing compared to before.

--- a922a8bc-6602-44d4-8ab2-a4062fc64d99/Kv-FlaggGenerering/RunConfig/fegetOmhandertagande.sql
+++ a922a8bc-6602-44d4-8ab2-a4062fc64d99/Kv-FlaggGenerering/RunConfig/fegetOmhandertagande.sql
@@ -19,59 +19,7 @@ fastighetsYtor as (select *
 		from sde_miljo_halsoskydd.gng.MoH_Slam_Lokalt_p_evw LOKALT_SLAM_P
               )
 
-     select * into #indexeratLokalt from LOKALT_SLAM_P
-end
-;
-go
-    -- drop PK constraint if it exists
-IF EXISTS (SELECT * FROM sys.key_constraints WHERE type = 'PK' AND parent_object_id = OBJECT_ID('tempDb..#indexeratLokalt') AND Name = 'PK_YourTable')
-   ALTER TABLE #indexeratLokalt
-   DROP CONSTRAINT PK_YourTable
-GO
-
--- drop column if it already exists
-IF EXISTS (SELECT * FROM sys.columns WHERE Name = 'RowId' AND object_id = OBJECT_ID('tempDb..#indexeratLokalt'))
-    ALTER TABLE #indexeratLokalt DROP COLUMN RowId
-GO
-
--- add new "RowId" column, make it IDENTITY (= auto-incrementing)
-ALTER TABLE #indexeratLokalt
-ADD RowId INT IDENTITY(1,1)
-GO
-
--- add new primary key constraint on new column
-ALTER TABLE #indexeratLokalt
-ADD CONSTRAINT PK_YourTable
-PRIMARY KEY CLUSTERED (RowId)
-GO
-    ;
-    CREATE SPATIAL INDEX si_tbSpatialTable__geometry_col
-	   ON #indexeratLokalt(shape)
-	   WITH
-	   (
-	         BOUNDING_BOX= (xmin=-20, ymin=50, xmax=-19, ymax=51),
-	         GRIDS= ( LEVEL_3= HIGH, LEVEL_2 = HIGH )
-	   );
-
-
-with 
-		fasWithShape as (select fa.FNR,fa.BETECKNING fastighet , fa.TRAKT, yt.Shape from sde_geofir_gotland.gng.FA_FASTIGHET fa inner join sde_gsd.gng.REGISTERENHET_YTA yt on fa.FNR = yt.FNR_FDS)
-
-              ,geoFast as (
-	   select Fastighet_,Eget_omhän,Lokalt_omh,Fastighe00,Beslutsdat,Diarienr,Anteckning,uf2.Shape,coalesce(sYt.FAStighet,uf2.Fastighet) fastighet,syt.shape FaShape
-	   from  #indexeratLokalt uf2
-	    inner join
-       		fasWithShape sYt on sYt.shape.STIntersects(uf2.Shape) = 1)
-
-    select * into ##temp2 from geoFast
-;
-    declare @outString as nvarchar(max) = (Select top 1 count(*) from #temp2)
-;
-THROW 51000, @outString, 1;
-/*
-       ,utan_fastighet2 as (select * from geoFast where FaShape is null)
-
-       ,noShapeLikeMatch as (select Diarienr
+           ,noShapeLikeMatch as (select Diarienr
 				   , fy.Fastighet
 				   , Fastighet_
 				   , Fastighe00
@@ -80,27 +28,9 @@ THROW 51000, @outString, 1;
 				   , Beslutsdat
 				   , Eget_omhän
 				   , fy.shape
-			      from utan_fastighet2 wf
+			      from LOKALT_SLAM_P wf
 				  left outer join fastighetsYtor fY
 				  on wf.Fastighet like '%' + fy.FAStighet + '%')
-
-
-       ,Med_fastighet2 as (select * from noShapeLikeMatch where Fastighet is not null)
-
-      ,MedOchUtanFas as (select 				Diarienr, Fastighet, Fastighet_, Fastighe00, Lokalt_omh, Anteckning, Beslutsdat, Eget_omhän, shape
-			 from Med_fastighet2 union all select 	Diarienr, FAStighet, Fastighet_, Fastighe00, Lokalt_omh, Anteckning, Beslutsdat, Eget_omhän, Shape
-			 from geoFast )
-
-
-
-
-	,FiltreraPerSocken as (
-	    select uf2.* from MedOchUtanFas uf2  inner join
-       		fastighetsYtor syt
-       		 on sYt.shape.STIntersects(uf2.Shape) = 1
-
-	    )
-
   ,egetOmh as (
 	      select row_number() over (partition by Fastighet order by Beslutsdat) Nr,
 	             fastighet,shape,
@@ -108,8 +38,7 @@ THROW 51000, @outString, 1;
 		       nullif(ltrim(Fastighet_)+' - ',' - '), nullif(ltrim(Eget_omhän)+' - ',' - '),
 			nullif(ltrim(Lokalt_omh)+' - ',' - '), nullif(ltrim(Anteckning)+' - ',' - '),
 		       FORMAT(Beslutsdat,' yyyy-MM-dd')) egetOmhändertangandeInfo
-		       from FiltreraPerSocken  sYMfuf)
-
+		       from noShapeLikeMatch  sYMfuf)
        ,egetOmhy as (
 	      select fastighet,shape, nr
 				      , STUFF((
